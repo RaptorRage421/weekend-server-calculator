4d3d3d67cb5840e530f56c15a9f3908bb74b239f
Sam Shacter <sshacter@gmail.com>
2024-05-10 20:29:10 -0500

    done a lot with the math logic. setup the post route so i could test the logic with postman data injections

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,27 @@
 console.log('client.js is sourced!');
+let calculatorInput = (event) => {
+    event.preventDefault()
+    // console.log("in add item")
+  let pressedOperator = []
+    axios({
+      method: 'POST',
+      url: '/calculations',
+      data: {
+        numOne: parseFloat(document.getElementById('number_one').value),
+        numTwo: parseFloat(document.getElementById('number_two').value),
+        operator: document.getElementById('plus').value
+      }
+    })
+      .then((response) => {
+        // console.log("SUCCESS")
+  
+
+       //! call the GET function in here
+      })
+      .catch((error) => {
+        console.log('POST for /calculations has not been added.')
+        alert("Ooooopsies Add new Guess Failed")
+      })
+    document.getElementById("inputForm").reset()
+  }
+   
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 let PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(express.static('server/public'));
@@ -9,13 +12,50 @@ app.use(express.static('server/public'));
 // calculation objects:
 let calculations = []
 
+let computeAnswer = (numOne,numTwo,operator) => {
+let storageSpot = {}
+storageSpot.numOne = parseFloat(numOne)
+storageSpot.numTwo = parseFloat(numTwo)
+storageSpot.operator = operator
+if (operator == "+"){
+storageSpot.answer = (Number(numOne) + Number(numTwo))
+console.log(storageSpot)
+calculations.push(storageSpot)
+}
+if (operator == "-"){
+  storageSpot.answer = (Number(numOne) - Number(numTwo))
+  console.log(storageSpot)
+  calculations.push(storageSpot)
+}
+if (operator == "*"){
+  storageSpot.answer = (Number(numOne) * Number(numTwo))
+  console.log(storageSpot)
+  calculations.push(storageSpot)
+}
+if (operator == "/"){
+  storageSpot.answer = (Number(numOne) / Number(numTwo))
+  console.log(storageSpot)
+  calculations.push(storageSpot)
+}
 
+console.log(calculations)
+}
 // Here's a wonderful place to make some routes:
 
 // GET /calculations
-
+app.get('/calculations', (req, res) => {
+  //! this is where the calculation variables get stored
+  res.send(calculations)
+})
 // POST /calculations
+app.post('/calculations', (req, res) => {
+  let incomingComputation = req.body
+  //! can call a function here with server side logic to compare the inputs and do math.
+  console.log("These are the variables of the calculation: " , incomingComputation)
+ computeAnswer(incomingComputation.numOne,incomingComputation.numTwo,incomingComputation.operator)
+  res.sendStatus(201)
 
+})
 
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
