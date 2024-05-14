5936c2ce7f75b57fb49e4568b2c6eb4f27c5fcdd
Sam Shacter <sshacter@gmail.com>
2024-05-14 00:26:25 -0500

    fixing initial GET error when loading without results present

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -168,17 +168,18 @@ function renderCalculations(calcObject) {
 
     newCalcSpot.innerHTML = ""
     calculationHistory.innerHTML = ""
+    console.log('calcObject', calcObject)
     calcObject.reverse()
     // newCalcSpot.innerHTML += `<div>${calcObject[calcObject.length].numOne} ${calcObject[calcObject.length].operator} ${calcObject[calcObject.length].numTwo} = ${calcObject[calcObject.length].answer}</div>`
-    // if (newCalcSpot.resut){
+    if (calcObject[0].result !== undefined){
     document.getElementById('calculatorscreen').value = `${calcObject[0].result}`
     newCalcSpot.innerHTML += `<div><h1><b>${calcObject[0].result}</b></h1></div>`
-// }
+
     for (let i = 0; i < calcObject.length; i++) {
 
         console.log(`${calcObject[i].numOne} ${calcObject[i].operator} ${calcObject[i].numTwo} = ${calcObject[i].result}`)
 
         calculationHistory.innerHTML += `<div>${calcObject[i].numOne} ${calcObject[i].operator} ${calcObject[i].numTwo} = ${calcObject[i].result}</div>`
     }
-
+}
 }
